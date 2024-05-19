6c8a0c567e82f62ad6babd88d6775758e8392fd8
David Drysdale <dmd@lurklurk.org>
2024-05-19 12:40:42 +0100

    skreate-wasm: minimize code

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -4,21 +4,17 @@
 use log::info;
 use wasm_bindgen::prelude::*;
 
-// Javascript functionality invoked from Rust.
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
 // Rust functionality invoked from Javascript.
 
 /// Perform initialization. Safe to be invoked more than once.
 #[wasm_bindgen]
 pub fn initialize() {
     wasm_logger::init(wasm_logger::Config::default());
-    info!("initialize: set panic hook");
-    set_panic_hook();
+    #[cfg(feature = "console_error_panic_hook")]
+    {
+        info!("initialize: set panic hook");
+        console_error_panic_hook::set_once();
+    }
 }
 
 /// Generate output.
@@ -26,11 +22,4 @@ pub fn initialize() {
 pub fn generate(input: &str) -> Result<String, JsError> {
     info!("In generate('{input}')");
     Ok(skreate::generate(input).expect("TODO: map error"))
-}
-
-// Rust interactions with WASM.
-
-fn set_panic_hook() {
-    #[cfg(feature = "console_error_panic_hook")]
-    console_error_panic_hook::set_once();
 }
